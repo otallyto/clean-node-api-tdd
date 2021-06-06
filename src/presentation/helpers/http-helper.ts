32f81abc84aadae1c05693000a42b630398b9a80
otallyto <rodrigues.tallyto@hotmail.com>
2021-06-06 19:13:30 -0300

    refactor: move server error to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,8 +1,16 @@
+import { ServerError } from '../errors/server-error'
 import { HttpResponse } from '../protocols/http'
 
 export const badRequest = (error: Error): HttpResponse => {
   return {
     statusCode: 400,
     body: error
+  }
+}
+
+export const serverError = (): HttpResponse => {
+  return {
+    statusCode: 400,
+    body: new ServerError()
   }
 }
