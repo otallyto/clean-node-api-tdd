9b86062518ccd5f9fc17776c4ad6c11fd1239129
otallyto <rodrigues.tallyto@hotmail.com>
2021-06-06 11:12:50 -0300

    chore: add npm

--- gitCommands.sh
+++ gitCommands.sh
@@ -3,3 +3,10 @@ git config --list
 git config --global --edit
 
 git config --global core.editor code
+
+[core]
+	editor = code --wait
+[alias]
+	s = !git status -s
+	l = !git log --pretty=format:'%C(blue)%h %C(red)%d %C(white)%s - %C(cyan)%cn, %C(green)%cr'
+	c = !git add --all && git commit -m
