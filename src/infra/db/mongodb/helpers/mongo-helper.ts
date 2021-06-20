eabeb51a456301cf96f6f0089a6f6d8f36d7a47f
otallyto <rodrigues.tallyto@hotmail.com>
2021-06-20 19:42:10 -0400

    feat: ensure AccountMongoRepository returns an account on sucess

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,16 +1,18 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
-export class MongoHelper {
-  client: MongoClient
-
+export const MongoHelper = {
+  // @ts-expect-error
+  client: null as MongoClient,
   async connect (url: string): Promise<any> {
     this.client = await MongoClient.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
-  }
-
+  },
   async disconnect (): Promise<any> {
     await this.client.close()
+  },
+  getColletion (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
