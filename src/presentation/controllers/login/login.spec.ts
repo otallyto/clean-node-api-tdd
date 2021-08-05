6f24c87f4f28561fba9e270c2d4dda50b4e6e5b0
otallyto <rodrigues.tallyto@hotmail.com>
2021-08-05 20:13:06 -0300

    test: ensure LoginController returns 500 if EmailValidator trhows

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { EmailValidator, HttpRequest } from '../signup/signup-protocols'
 import { LoginController } from './login'
 
@@ -21,7 +21,7 @@ const makeSut = (): SutTypes => {
 
   return { sut, emailValidatorStub }
 }
-const makeFakeRquest = (): HttpRequest => {
+const makeFakeRequest = (): HttpRequest => {
   return {
     body: {
       password: 'any_password',
@@ -55,7 +55,7 @@ describe('Login Controller', () => {
   it('Should return 400 if an invalid email is provided', async () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValue(false)
-    const httpRequest = makeFakeRquest()
+    const httpRequest = makeFakeRequest()
     const httpResponse = await sut.handler(httpRequest)
     expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
   })
@@ -63,8 +63,14 @@ describe('Login Controller', () => {
   it('Should call EmailValidator with correct email', async () => {
     const { sut, emailValidatorStub } = makeSut()
     const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
-    const httpRequest = makeFakeRquest()
+    const httpRequest = makeFakeRequest()
     await sut.handler(httpRequest)
     expect(isValidSpy).toHaveBeenCalledWith(httpRequest.body.email)
+  })
+  it('Should return 500 if EmailValidator throws', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementation(() => { throw new Error() })
+    const httpResponse = await sut.handler(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
